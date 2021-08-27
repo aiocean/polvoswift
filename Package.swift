635ea32cc1c26cff9679e63c28baf5c434fb3a90
Duoc Nguyen <nguyenvanduocit@gmail.com>
2021-08-27 07:55:05 +0700

    [WIP] init

--- Package.swift
+++ Package.swift
@@ -14,13 +14,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(name: "grpc-swift", url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "polvoswift",
-            dependencies: []),
+            dependencies: [
+                "SwiftProtobuf",
+                .product(name: "GRPC", package: "grpc-swift")
+            ]),
         .testTarget(
             name: "polvoswiftTests",
             dependencies: ["polvoswift"]),
